6d1ec2b71eace1a904546c32a8d463972e2b76e4
yashwalhekar <yashwalhekar371@gmail.com>
2025-02-27 16:18:15 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor"; // Moves dependencies to a separate chunk
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // (Optional) Adjust chunk warning limit
-  },
+  plugins: [react()],
 })
