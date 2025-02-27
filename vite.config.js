7e89d0012fe96e8f765f96f7b0c7114eb8ed3010
yashwalhekar <yashwalhekar371@gmail.com>
2025-02-27 16:32:21 +0530

    about us

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "build", // Ensure Render finds the correct folder
+    chunkSizeWarningLimit: 2000, // Adjust warning limit
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Separate vendor files
+          }
+        },
+      },
+    },
+  },
+});
